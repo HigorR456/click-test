5b83fda6c131753dcd15b4284f48f457056e4763
HigorR456 <higor.ruan456@gmail.com>
2022-12-25 10:08:54 -0300

    Update
    update the App.tsx and App.css and delete components folder with all files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,76 @@
 import { useState } from 'react'
-import { Print } from './components/printCircle'
-import { CircleDiv } from './components/div'
 import './App.css'
 
 function App() {
 
-  let storage = [];
-  const [num, setPrint] = useState()
+  //undo cordinates storage
+  const [undoStorage, setUndoStorage] = useState<any[]>([])
 
-  function Do() {
-    console.log(storage)
-    divWrapper.insertAdjacentHTML('afterend', storage)
+  //click cordinates storage
+  const [storage, setStorage] = useState<any[]>([]);
+
+  //click component
+  const getCordinates = (event: any) => {
+    const currentCordinates = {
+      X: event.clientX,
+      Y: event.clientY,
+    };
+
+    setStorage((prev) => [...prev, currentCordinates])
+
+    setUndoStorage([])
   }
-  
 
-  function Undo(){
-    const circleWrap = document.querySelector('.circle')
+  //undo component
+  const handleUndo = (event: any) => {
+    event.stopPropagation();
 
-    storage.push(circleWrap)
-    
-    circleWrap.remove()
+    if (storage.length === 0) {
+      return;
+    }
+
+    const prevItem = storage[storage.length - 1]
+    setUndoStorage((prev) => [...prev, prevItem])
+
+    setStorage((prev) => {
+      const cordinatesCopy = [...prev].slice(0, -1);
+      return cordinatesCopy;
+    })
   }
 
-return (
-  <>
+  //redo component
+  const handleRedo = (event: any) => {
+    event.stopPropagation();
 
-    <button onClick={Undo}>Undo</button>
-    <button onClick={Do}>Do</button>
-    
-    <CircleDiv />
-    
-    
-  </>
-)
+    if (undoStorage.length === 0) {
+      return;
+    }
+
+    const redoCircle = undoStorage[undoStorage.length - 1]
+    setStorage((prev) => [...prev, redoCircle])
+
+    setUndoStorage((prev) => {
+      const cordinatesCopy = [...prev].slice(0, -1);
+      return cordinatesCopy;
+    })
+  }
+
+  return (
+    <>
+
+      <div className='circleWrapper' onClick={getCordinates}>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
+        {storage.map((item) => (
+          <span
+            key={item.X}
+            className='circle'
+            style={{ top: item.Y, left: item.X }} />
+        ))}
+      </div>
+
+    </>
+  )
 }
 
 export default App
